3f82ddf57332dee313a355f21e1d851972507ad7
Udit Kumar <surajgautam783792@gmail.com>
2024-02-23 22:02:42 +0530

    Adding Dom File

---
+++ index.js
@@ -0,0 +1,45 @@
+// // DOM Manipulation
+
+// // 1st is GetElementById------------------------------------------>>>>>
+// // document.getElementById("ok");
+// // let head = document.getElementById("ok");
+// // console.log(head);
+// // console.dir(head);
+
+// // 2nd is GetElementByClass------------------------------------------>>>>>
+// // document.getElementsByClassName(".hello");
+// // let heads = document.getElementsByClassName("hello");
+// // console.log(heads);
+// // console.dir(heads);
+
+// // 3rd is GetElementByTagName------------------------------------------>>>>>
+// // let parahas = document.getElementsByTagName("p");
+// // console.log(parahas);
+// // console.dir(parahas);
+
+// // QuerySelector (Return Nodes)
+// // let Firstelements = document.querySelector(".hello"); /// return 1st Element
+// // console.log(Firstelements);
+
+// // // QuerySelectorAll
+// // let Allelements = document.querySelectorAll(".hello"); /// return All Element
+// // console.log(Allelements);
+
+// // Propoerties
+// //Tag Name
+// // let dt = document.getElementById("ok");
+// // console.log(dt);
+// //dt.tagName You Will get The Tag Name Of This Element
+
+// // Inner Text
+// let tdt = document.getElementById("ok");
+// console.log(tdt.innerText);
+
+// // Inner HTML
+// let dtt = document.getElementById("ok");
+// console.log(dtt.innerHTML);
+
+//  Text-Content
+// let tdt = document.getElementById("ok");
+// console.log((tdt.innerText = "Hello World"));
+// console.log((tdt.innerHTML = <h2>Inner H2 Tag</h2>));
